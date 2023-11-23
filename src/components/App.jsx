81e85134b130c5e844e0fb72669cdb4618bcb341
ritttttkkkaaa <mo659835@gmail.com>
2023-11-23 13:22:13 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { findImage } from './services/API';
 import Searchbar from './Searchbar/Search';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -7,123 +7,95 @@ import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    error: null,
-    querry: '',
-    modalImageUrl: null,
-    isModalOpen: false,
-    page: 1,
-    hasMoreImages: true,
-  };
+export function App()  {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [querry, setQuerry] = useState('');
+    const [modalImageUrl, setModalImageUrl] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [page, setPage] = useState(1);
+    const [hasMoreImages, setHasMoreImages] = useState(true);
+  
+    useEffect(() => {
+      if (querry === '') return;
+      const FetchPostByQuerry = async () => {
+        try {
+          setIsLoading(true);
+          const newImages = await findImage(querry, page);
+          if (newImages.length === 0) {
+            alert(
+              'Oops',
+              `Sorry, no images found for the querry "${querry}"`,
+              'error'
+            );
+            return;
+          }
+          setImages(prevImages => [...prevImages, ...newImages]);
+          setHasMoreImages(newImages.length >= 12);
+        } catch (error) {
+          setError(error.message);
+          alert('Error', 'Error 404 - "Not Found"', 'error');
+        } finally {
+          setIsLoading(false);
+        }
+      };
+  
+      FetchPostByQuerry();
+    }, [querry, page]);
 
-  FetchPostByQuerry = async () => {
-    try {
-      this.setState({ isLoading: true, images: [] });
-      const images = await findImage(this.state.querry, this.state.page);
-      if (images.length === 0) {
-        alert(
-          'Oops',
-          `Sorry, no images found for the query "${this.state.querry}"`,
-          'error'
-        );
-      } else {
-        const newImages = [...this.state.images, ...images];
-        this.setState({
-          images: newImages,
-          page: this.state.page + 1,
-          hasMoreImages: newImages.length >= 12,
-        });
-      }
-    } catch (error) {
-      this.setState({ error: error.message });
-      alert('Error', 'Error 404 - "Not Found"', 'error');
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  loadMoreImages = () => {
-    this.setState({ isLoading: true });
-
-    findImage(this.state.querry, this.state.page)
-      .then(newImages => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...newImages],
-          page: prevState.page + 1,
-          isLoading: false,
-        }));
-      })
-      .catch(error => {
-        this.setState({
-          error: error.message,
-          isLoading: false,
-          hasMoreImages: false,
-        });
-      });
-  };
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.querry !== this.state.querry) {
-      this.setState({ page: 1 });
-      this.FetchPostByQuerry();
-    }
-  }
-
-  handleSearchSubmit = e => {
-    e.preventDefault();
-    this.setState({ page: 1 });
-    const querry = e.currentTarget.elements.searchImage.value;
-    this.setState({ querry });
-    e.currentTarget.reset();
-  };
-
-  openModal = imageUrl => {
-    this.setState({
-      modalImageUrl: imageUrl,
-      isModalOpen: true,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      modalImageUrl: null,
-      isModalOpen: false,
-    });
-  };
-
-  render() {
-    const showPosts = this.state.images.length > 0;
+    const loadMoreImages = () => {
+      setPage(prev => prev + 1);
+    };
+  
+    const handleSearchSubmit = e => {
+      e.preventDefault();
+      setPage(1);
+      const query = e.currentTarget.elements.searchImage.value;
+      setQuerry(query);
+      setImages([]);
+      e.currentTarget.reset();
+    };
+  
+    const openModal = imageUrl => {
+      setModalImageUrl(imageUrl);
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalImageUrl(null);
+      setIsModalOpen(false);
+    };
+  
+    const showPosts = images.length > 0;
 
     return (
       <>
-        <Searchbar handleSearchSubmit={this.handleSearchSubmit} />
-        {this.state.isLoading && <Loader />}
-        {this.state.error ? (
-          this.setState({ hasMoreImages: false })
+        <Searchbar handleSearchSubmit={handleSearchSubmit} />
+        {isLoading && <Loader />}
+        {error ? (
+          setHasMoreImages(false)
         ) : (
           <ImageGallery
-            images={this.state.images}
-            openModal={this.openModal}
+            images={images}
+            openModal={openModal}
             showPosts={showPosts}
           />
         )}
-        {showPosts && this.state.hasMoreImages && (
+        {showPosts && hasMoreImages && (
           <Button
-            onClick={this.loadMoreImages}
-            showButton={!this.state.isLoading}
+            onClick={loadMoreImages}
+            showButton={!isLoading}
           />
         )}
-        {this.state.isModalOpen && (
+        {isModalOpen && (
           <Modal
-            isOpen={this.state.isModalOpen}
-            imageUrl={this.state.modalImageUrl}
-            onClose={this.closeModal}
+            isOpen={isModalOpen}
+            imageUrl={modalImageUrl}
+            onClose={closeModal}
           />
         )}
       </>
     );
   }
-}
+
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,35 @@
-import React, { Component } from 'react';
-import css from './Modal.module.css';
-import  {createPortal}  from 'react-dom';
+import React, { useEffect } from 'react';
 
-const modalRoot = document.querySelector('#modal-root');
+function Modal({ isOpen, imageUrl, onClose }) {
+  useEffect(() => {
+    const handleKeyPress = e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyPress);
     }
-  };
 
-  handleBackDropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      if (isOpen) {
+        document.removeEventListener('keydown', handleKeyPress);
+      }
+    };
+  }, [isOpen, onClose]);
 
-  render() {
-
-    const { imageUrl} = this.props;
-    return createPortal(
-      <div className={css.overlay} onClick={this.handleBackDropClick}>
-        <div className={css.modal}>
-          <img src={imageUrl} alt='img' />
+  return (
+    <>
+      {isOpen && (
+        <div className="Overlay" onClick={onClose}>
+          <div className="Modal">
+            <img src={imageUrl} alt="" />
+          </div>
         </div>
-      </div>,
-      modalRoot
-    );
-  }
+      )}
+    </>
+  );
 }
 
 export default Modal;
